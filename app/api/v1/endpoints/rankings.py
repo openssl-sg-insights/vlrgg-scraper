888cfb7120f993cdf0921d23549f8d9a5aa0af30
Akhil Narang <me@akhilnarang.dev>
2022-08-27 12:52:12 +0530

    Revert "feat(rankings): return the older way for older app versions"
    Shouldn't be there in the code _forever_ I feel
    
    This reverts commit 4994a4f2bbc6c95f9d633d99a14709f8efcf117f.

--- app/api/v1/endpoints/rankings.py
+++ app/api/v1/endpoints/rankings.py
@@ -1,9 +1,7 @@
 import json
-import logging
 from typing import Any
 
-import semver
-from fastapi import APIRouter, Header
+from fastapi import APIRouter
 
 from app import cache, schemas
 from app.services import rankings
@@ -12,21 +10,8 @@ router = APIRouter()
 
 
 @router.get("/", response_model=list[schemas.Ranking])
-async def get_rankings(app_version: str | None = Header(None)) -> Any:
+async def get_rankings() -> Any:
     try:
-        response = [schemas.Ranking.parse_obj(ranking) for ranking in json.loads(await cache.get("rankings"))]
+        return [schemas.Ranking.parse_obj(ranking) for ranking in json.loads(await cache.get("rankings"))]
     except cache.CacheMiss:
-        response = await rankings.ranking_list()
-
-    # TODO: revert after a month or so
-    if app_version and semver.compare("0.2.12", app_version[1:]) > -1:
-        ranking_replace = {
-            "Asia-Pacific": "Asia Pacific",
-            "Latin America South": "Latin America - South",
-            "Latin America North": "Latin America - North",
-        }
-        for ranking in response:
-            if region := ranking_replace.get(ranking.region):
-                ranking.region = region
-
-    return response
+        return await rankings.ranking_list()
